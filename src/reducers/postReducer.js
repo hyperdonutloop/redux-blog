ce81a77dcb1c81bf12ddd747ee60306a90e8d0dd
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-21 23:14:06 -0800

    built out first reducer

---
+++ src/reducers/postReducer.js
@@ -0,0 +1,17 @@
+const postReducer = (state = [], action) => {
+  switch(action.type) {
+    case 'ADD_POST':
+      return state.concat([action.data]);
+    default:
+      return state;
+  }
+}
+
+export default postReducer;
+
+// we are calling the switch statement (a function) 
+// and we are switching based on the value of `action.type`
+// If the value is ADD_POST then we are returning a new array (line 1: state = [])
+// containing the action.data. Action.data = an object with individual post title and message.
+// the reducer expects default value for state, hence an empty array
+
