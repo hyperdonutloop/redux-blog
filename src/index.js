ac282d40f29c7edc22198b7c69b50c786bd4ae5f
donutwizard666 <thebeardedwarrior@icloud.com>
2020-02-21 23:20:19 -0800

    wrapped app in provider and gave App access to store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,18 @@ import './index.css';
 import App from './App';
 
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-const store = createStore();
+import postReducer from './reducers/postReducer.js';
+
+const store = createStore(postReducer);
 
 ReactDOM.render(
-<App />, document.getElementById('root'));
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
 
 
+// the Provider uses React context which allows you to pass the store object to any components that need to access it without having to pass props.
+// When we wrap the APP component (the parent) with the Provider, all child components get access to the store
